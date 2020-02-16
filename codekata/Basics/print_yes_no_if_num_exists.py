16996c034a7eed9a6d0b525bd892139528c9386e
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-02-16 06:04:56 -0800

    Create print_yes_no_if_num_exists.py

---
+++ codekata/Basics/print_yes_no_if_num_exists.py
@@ -0,0 +1,13 @@
+#Given 2 numbers print yes if number exists
+
+N,K=map(int,input().split())
+n=list(map(int,input().split()))
+count=0
+for i in range(len(n)):
+	if(n[i]==K):
+		count=count+1
+if(count!=0):        
+	print("yes")
+else:
+	print("no")  
+      
