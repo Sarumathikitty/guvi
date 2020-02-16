6e42862496d3468874f44326162337efc656f91f
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-02-16 05:59:23 -0800

    Create form_sides_triangle.py

---
+++ codekata/Basics/form_sides_triangle.py
@@ -0,0 +1,12 @@
+#Given three numbers if they form sides of triangle
+
+def checkvalidity(a,b,c):
+	if(a+b<=c) or (a+c<=b) or (b+c<=a):
+		return False
+	else:
+		return True
+a,b,c=map(int,input().split())
+if checkvalidity(a,b,c):
+	print("yes")
+else:
+	print("no")
