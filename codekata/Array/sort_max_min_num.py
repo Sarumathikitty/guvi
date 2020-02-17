5745893a1c2073d12a84ddc5898de388b3219006
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-02-16 16:59:45 -0800

    Create sort_max_min_num.py

---
+++ codekata/Array/sort_max_min_num.py
@@ -0,0 +1,11 @@
+#sort number by maximum and minimum number
+
+N=int(input())
+num1=list(map(int,input().split()))
+m=sorted(num1)
+a=m[::-1]
+s=[]
+for i in range(N):
+	s.append(a[i])
+	s.append(m[i])
+print(*s[:N])
