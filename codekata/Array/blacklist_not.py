b04000ac3baa1b9bbbd065b79887861efcdb651c
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-02-16 16:41:15 -0800

    Create blacklist_not.py

---
+++ codekata/Array/blacklist_not.py
@@ -0,0 +1,16 @@
+#find out blacklisted or not
+def function(n,s):
+	count=0
+	for i in range(len(s)):
+		if(s[i]=="P"):
+			count+=1
+	a=float(count/n)*100
+	return(a)
+
+
+n=int(input())
+s=list(map(str,input().split()))
+c=function(n,s)
+if(c>25):
+	print("Not Blacklisted")
+else:
