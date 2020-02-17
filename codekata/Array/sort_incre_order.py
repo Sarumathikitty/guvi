ced49281093e649045fb96ac1a7ad3f186310e0b
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-02-16 16:49:06 -0800

    Create sort_incre_order.py

---
+++ codekata/Array/sort_incre_order.py
@@ -0,0 +1,11 @@
+#Given a number sort in increasing order
+
+N=int(input()) 
+n1=list(map(int,input().split())) 
+n2=sorted(n1) 
+a=[]
+for i in range(N):
+    for j in range(N): 
+        if(n2[i]==n1[j]):
+             a.append(j+1) 
+print(*a) 
