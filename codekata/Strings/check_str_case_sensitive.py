9d25244116f37ec37d8f5999689de537904cbfec
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-02-16 17:36:33 -0800

    Create check_str_case_sensitive.py

---
+++ codekata/Strings/check_str_case_sensitive.py
@@ -0,0 +1,12 @@
+#Given a string check they are case sensitive
+
+s1,s2=map(str,input().split())
+count=0
+if(len(s1)==len(s2)):
+	for i in range(len(s1)):
+		if(s1[i]==s2[i]):
+			count+=1
+if(count==len(s2)):
+	print("yes")
+else:
+	print("no")
