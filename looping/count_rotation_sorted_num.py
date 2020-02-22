6b1c9f56c5fe3eebbdff2e32ab8cf2a0fb11eadb
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-02-21 20:59:01 -0800

    Create count_rotation_sorted_num.py

---
+++ looping/count_rotation_sorted_num.py
@@ -0,0 +1,12 @@
+#find count k of rotated sorted array
+
+n=int(input())
+a=input().split()
+b=input().split()
+s1=''
+s2=''
+for i in range(n):
+    s1+=a[i]
+    s2+=b[i]
+c=s1+s1
+print(c.index(s2))
