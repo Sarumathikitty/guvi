050f5ea3cabdadc0b6f80cd57360896ab3500a7c
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-02-25 23:22:26 -0800

    Create prime_num.js

---
+++ javascript/anonymous function/prime_num.js
@@ -0,0 +1,10 @@
+#return all prime numbers
+
+const Array = [1, 3, 2, 5, 10];
+const PrimeArray =Array.filter(function(num){
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) return false;
+  }
+  return num !== 1;
+});
+console.log(PrimeArray);
