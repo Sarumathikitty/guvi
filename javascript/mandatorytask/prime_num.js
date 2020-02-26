fadc477a978737992167d262172c6e13448bd07a
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-02-25 23:10:45 -0800

    Update prime_num.js

--- javascript/mandatorytask/prime_num.js
+++ javascript/mandatorytask/prime_num.js
@@ -1,13 +1,10 @@
 #return all prime numbers in array
 
 const newArray = [1, 3, 2, 5, 10];
-const isPrime = num => {
+const myPrimeArray = newArray.filter(num => {
   for (let i = 2; i < num; i++) {
     if (num % i === 0) return false;
   }
   return num !== 1;
-};
-
-
-const myPrimeArray = newArray.filter(element => isPrime(element));
+});
 console.log(myPrimeArray);
