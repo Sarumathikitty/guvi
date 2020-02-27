53b1c2c182b41ab645083c7bfe48699fac266502
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-02-26 21:24:24 -0800

    Create prgm1.js

---
+++ javascript/task/prgm1.js
@@ -0,0 +1,28 @@
+#create a div and change colour onmouse over
+
+var x = document.createElement("div"); 
+x.id="img";
+x.style.color="red";
+var t = document.createTextNode("Paragraph is created."); 
+x.appendChild(t);
+document.body.appendChild(x); 
+document.getElementById("img").onmouseover = function() {
+mouseOver()
+};
+function mouseOver() {
+  document.getElementById("img").style.color = "blue";
+}
+
+//html
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Create a div and change the color onmouse over</title>
+</head>
+<body>
+
+<script src="prgm1.js"></script>
+
+</body>
+</html>
