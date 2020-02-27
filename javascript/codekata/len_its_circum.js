0ca51656991feda05a9d8cb03cf144c8e3682ba5
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-02-27 06:14:27 -0800

    Create len_its_circum.js

---
+++ javascript/codekata/len_its_circum.js
@@ -0,0 +1,8 @@
+#circumference of number
+
+const readline = require('readline');
+const inp=readline.createInterface({input:process.stdin});
+inp.on("line",(data)=>{
+  circle=2*3.14159*data;
+  console.log(circle.toFixed(2));
+});
