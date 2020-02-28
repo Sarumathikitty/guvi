1adf4da4303c643b545f439832e3b2669b2e538f
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-02-27 17:58:45 -0800

    Create fact_num.js

---
+++ javascript/codekata/fact_num.js
@@ -0,0 +1,12 @@
+#factorial of number
+
+const readline = require('readline');
+const inp=readline.createInterface({input:process.stdin});
+inp.on("line",(data)=>{
+  var ans=1;
+  for(var i=2;i<=data;i++)
+  {
+    ans=ans*i;
+  } 
+  console.log(ans);
+});
