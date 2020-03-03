bde5dd5dcb133e27503c4a200cb34a836cdeff6e
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-03-03 00:56:20 -0800

    Create calculator.js

---
+++ javascript/task/calculator.js
@@ -0,0 +1,46 @@
+
+
+document.form.textview.value1=eval(sub)
+}
+else if(mul){
+document.form.textview.value2=eval(mul)
+}
+else if(div){
+document.form.textview.value3=eval(div)
+}
+}
+function clean(){
+document.form.textview.value="";
+}
+</script>
+</head>
+<body>
+<div class="main">
+<form name="form">
+<input class="textview" name="textview">
+</form>
+<table>
+<tr>
+<td><input class="button" type="button"value="C" onclick="clean()"></td>
+<td><input class="button" type="button"value="0" onclick="insert(0)"></td>
+<td><input class="button" type="button"value="/" onclick="insert('/')"></td>
+<td><input class="button" type="button"value="*" onclick="insert('*')"></td>
+</tr>
+<tr>
+<td><input class="button" type="button"value="7" onclick="insert(7)"></td>
+<td><input class="button" type="button"value="8" onclick="insert(8)"></td>
+<td><input class="button" type="button"value="9" onclick="insert(9)"></td>
+<td><input class="button" type="button"value="-" onclick="insert('-')"></td>
+</tr>
+<tr>
+<td><input class="button" type="button"value="4" onclick="insert(4)"></td>
+<td><input class="button" type="button"value="5" onclick="insert(5)"></td>
+<td><input class="button" type="button"value="6" onclick="insert(6)"></td>
+<td><input class="button" type="button"value="+" onclick="insert('+')"></td>
+</tr>
+<tr>
+<td><input class="button" type="button"value="1" onclick="insert(1)"></td>
+<td><input class="button" type="button"value="2" onclick="insert(2)"></td>
+<td><input class="button" type="button"value="3" onclick="insert(3)"></td>
+<td><input class="button" type="button"value="=" onclick="equal()"></td>
+</tr>
