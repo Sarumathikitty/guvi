f1f8a0362df3ea7e54f4c35a70ea7654c02b87fa
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-03-04 03:34:54 -0800

    Create area_equ_triangle.js

---
+++ javascript/codekata/area_equ_triangle.js
@@ -0,0 +1,7 @@
+#find area of equilateral triangle
+
+const readline = require('readline');
+const inp=readline.createInterface({input:process.stdin});
+inp.on("line",(data)=>{
+  console.log(circle.toFixed(2));
+});
