4ac76ae13e47ee40b7b16983f9893feb2e2911b6
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-03-05 02:56:24 -0800

    Create prgm3.js

---
+++ javascript/task/prgm3.js
@@ -0,0 +1,40 @@
+#email and password login
+
+var a=document.createElement("div");
+var s=document.createTextNode("Email:");
+a.appendChild(s);
+document.body.appendChild(a);
+
+var x = document.createElement("INPUT");
+x.id="m";
+x.setAttribute("type", "text");
+a.append(x);
+document.body.appendChild(a);
+
+var b=document.createElement("div");
+b.id="img";
+var c=document.createTextNode("Password:");
+b.appendChild(c);
+document.body.appendChild(b);
+
+var y= document.createElement("INPUT");
+y.id="l";
+y.setAttribute("type", "password");
+b.append(y);
+document.body.appendChild(b);
+
+var z=document.createElement("button");
+var d=document.createTextNode("submit");
+z.append(d);
+document.body.appendChild(z);
+
+
+z.onclick=function(){
+checkform()
+};
+
+function checkform(){
+
+var emailvalue=document.getElementById("m").innerHTML;
+var passwordvalue=document.getElementById("l").innerHTML;
+}
