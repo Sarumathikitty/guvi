640e7a029a800024a5e42fdd91961991fc35aa4c
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-03-09 09:52:51 -0700

    Create swap_adjacent_element.js

---
+++ javascript/codekata/basics/swap_adjacent_element.js
@@ -0,0 +1,28 @@
+#swap adjacent elements
+
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+	userInput.push(data);
+});
+inp.on("close", () => {
+  var a=userInput[0].split(" ");
+  var n=userInput[1].split(" ");
+  var l=[]
+  for(i=0;i<a-1;i++)
+  {
+      var k=n[i+1]
+      var j=n[i]
+      l.push(k)
+      l.push(j)
+      i=i+1;
+  }
+  if(a%2!=0)
+  {
+    l.push(n[a-1])  
+  }
+	console.log(l.join(" "));
+});
