2a22ca5164f6f547c39ab9fdb58e2143db4a0d0e
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-04-06 20:54:01 -0700

    Update print_9th_table.js

--- javascript/codekata/print_9th_table.js
+++ javascript/codekata/print_9th_table.js
@@ -1,12 +1,11 @@
-# write a program to print the table of 9 
+# write a program to print the table of 9
 
 const readline=require('readline');
-const inp=readline.createInterface({input:process.stdin});
-inp.on("line" ,(data)=>{
-  var s="";
-  for(i=1;i<=data;i++)
-  {
-    s+=(i*9)+" ";
-  }
-  console.log(s);
-});  
+const inp=readline.createInterface({input:process.stdin})
+inp.on("line",(data)=>{
+    var a=[]
+    for(var i=1;i<=data;i++){
+     a.push(9*i)
+    }
+    console.log(a.join(' '));
+});
