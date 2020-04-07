6556ff92d2a8a0ab6ea9841eacb6dc3d5ad8b6dc
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-04-06 22:37:13 -0700

    Update bitwise_or_arr_elements.js

--- javascript/codekata/basics/bitwise_or_arr_elements.js
+++ javascript/codekata/basics/bitwise_or_arr_elements.js
@@ -1,13 +1,13 @@
-const readline = require ('readline');
-const inp=readline.createInterface({
-  input:process.stdin
-});
-const m=[];
+const readline=require('readline');
+const inp=readline.createInterface({input:process.stdin});
+const n=[]
 inp.on("line",(data)=>{
-  m.push(data);
+    n.push(data);
 });
 inp.on("close",()=>{
-  var A=m[0];
-  var B=m[1].split(" ");
-  console.log(parseInt(B[0]) & parseInt(B[1]) & parseInt(B[2]) & parseInt(B[3]));
+   var a=parseInt(n[0])
+   var b=n[1].split(' ')
+   var L=parseInt(b[0])
+   var R=parseInt(b[1])
+   console.log(L|R);
 });
