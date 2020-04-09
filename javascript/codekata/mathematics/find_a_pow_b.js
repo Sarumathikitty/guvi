10e2fd02c3119ad8f081ae3567a3032361b6dc8d
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-04-08 23:06:32 -0700

    Create find_a_pow_b.js

---
+++ javascript/codekata/mathematics/find_a_pow_b.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+userInput.push(data);
+});
+inp.on("close", () => {
+  var a=userInput[0].split(" ");
+  var b=parseInt(a[0]);
+  var v=parseInt(a[1]);
+  var c=Math.pow(b,v);
+console.log(c);
+});
