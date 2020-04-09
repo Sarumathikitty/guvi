fec20282fb92de2ba9e84b83455a84faac9369ee
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-04-08 23:24:34 -0700

    Create find_area.js

---
+++ javascript/codekata/mathematics/find_area.js
@@ -0,0 +1,11 @@
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+inp.on("line", (data) => {
+  var n=data.split(" ");
+  var a=parseInt(n[0]);
+  var b=parseInt(n[1]);
+  var c=(a*b)/2;
+	console.log(c)
+});
