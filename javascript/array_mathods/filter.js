e50c4b1913ceb9c4e4ea18b67a06fae6e49051f4
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-05-06 10:36:47 +0530

    Create filter.js

---
+++ javascript/array_mathods/filter.js
@@ -0,0 +1,26 @@
+const readline=require('readline');
+const inp=readline.createInterface({
+    input:process.stdin
+});
+const number=[];
+inp.on('line',(data)=>{
+    number.push(data);
+ });
+var a=[],d,c=[],result,sum=0;
+inp.on('close',()=>{
+    a=number[0].split(" ");
+function filter()
+{
+ for(var i=0;i<a.length;i++)
+ { 
+     if(a[i].length>7)
+  {
+    var answer=a[i];
+    c.push(answer);
+ }
+ }
+ return d=c.join(" ");
+ }
+result=filter();
+console.log(result);
+});
