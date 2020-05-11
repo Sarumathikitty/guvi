30615ef1cd68f12d6174e69aa356dbdbb867bbf4
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-05-11 08:43:40 +0530

    Create merge_and_print_sorted.js

---
+++ javascript/codekata/zoho/merge_and_print_sorted.js
@@ -0,0 +1,25 @@
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const a=[];
+inp.on("line", (data) => {
+  a.push(data);
+});
+inp.on("close",()=>{
+    var r=1,t=[]
+    for(var i=0;i<a.length-1;i++ )
+    {
+        
+        t.push(a[r])
+        r++
+    }
+    var p=t.join(" ")
+    var e=p.split(" ")
+    var u=e.sort(function(a,b){
+        return(a-b)
+    })
+    console.log(u.join(" "))
+    
+})
