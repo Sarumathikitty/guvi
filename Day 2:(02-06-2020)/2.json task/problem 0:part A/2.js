e63bf2c97027515686753951eb3b5d8065f275e7
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-06-02 06:39:10 -0700

    Create 2.js

---
+++ Day 2:(02-06-2020)/2.json task/problem 0:part A/2.js
@@ -0,0 +1,26 @@
+//Fluffy name is spelled wrongly. Update it to Fluffyy
+//create a object called cat
+var cat = {
+ name: "Fluffy",
+ activities: ['play', 'eat cat food'],
+ catFriends: [
+ {
+ name: 'bar',
+ activities: ['be grumpy', 'eat bread omblet'],
+ weight: 8,
+ furcolor: 'white'
+ }, 
+ {
+ name: 'foo',
+ activities: ['sleep', 'pre-sleep naps'],
+ weight: 3
+ }
+ ]
+}
+//create function called update
+function update(){
+    cat.name="fluffyy"
+//print updated fluffyy name
+console.log(cat);
+}
+update();
