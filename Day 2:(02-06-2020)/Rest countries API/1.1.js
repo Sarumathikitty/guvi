20190f1547234a352deaa365c1d85a1584967204
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-06-02 08:07:17 -0700

    Update 1.1.js

--- Day 2:(02-06-2020)/Rest countries API/1.1.js
+++ Day 2:(02-06-2020)/Rest countries API/1.1.js
@@ -1,1 +1,21 @@
+//1.1 Extract and print all the country name with the flag URL in console
+var request=new XMLHttpRequest();
 
+request.open('GET','https://restcountries.eu/rest/v2/all',true)
+
+
+request.onload=function(){
+
+    var jsondata=JSON.parse(this.response);
+ 
+   for(i=0;i<jsondata.length;i++)
+{
+
+    console.log(jsondata[i].flag);
+    console.log(jsondata[i].name);
+}
+
+
+}
+
+request.send();
