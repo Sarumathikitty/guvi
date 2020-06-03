04e44e108c1c7e97abb9e82ffc37573384cf8254
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-06-03 06:23:50 -0700

    Update 2.js

--- Day 3:(03-06-2020)/2.js
+++ Day 3:(03-06-2020)/2.js
@@ -1,1 +1,17 @@
-
+//programs in anonymous function
+//2.1. Print odd numbers in an array
+//create empty array
+a=[]
+var myArray=function(odd)
+{
+        for(var i=1; i<=10; i++){
+            //to check odd condition
+            if(i%2 !=0)
+            {
+              a.push(i);  
+            }
+    
+        }
+        console.log(a)
+}
+myArray(10);
