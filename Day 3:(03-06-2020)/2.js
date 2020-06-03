1333c10ab324a855d730c197c9b6280e320a1dca
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-06-03 07:37:48 -0700

    Update 2.js

--- Day 3:(03-06-2020)/2.js
+++ Day 3:(03-06-2020)/2.js
@@ -28,3 +28,20 @@ var arr=[38,56,78,89];
 var sum=add(arr);
 //print sum of array
 console.log(sum)
+
+
+// 2.4. Return all the prime numbers in an array
+//create a function
+var primenumArray = [2, 3, 4, 5, 6, 7, 8, 9, 10,  20]
+var prime=function(){
+primenumArray = primenumArray.filter((number) => {
+for (var i = 2; i <= Math.sqrt(number); i++) {
+if (number % i === 0) return false;
+}
+return true;
+});
+//print prime numbers
+ console.log(primenumArray);
+}
+prime()
+
