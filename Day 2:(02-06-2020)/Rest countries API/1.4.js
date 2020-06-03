626d7e7cf2c8a2444ba3029cb333c87b4a041d09
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-06-02 22:00:18 -0700

    Update 1.4.js

--- Day 2:(02-06-2020)/Rest countries API/1.4.js
+++ Day 2:(02-06-2020)/Rest countries API/1.4.js
@@ -1,5 +1,5 @@
 //1.4 Display the count of all countries sharing their border with the country name in
-console.
+//console.
 
 var request=new XMLHttpRequest();
 
@@ -8,7 +8,7 @@ request.onload=function(){
    var jsondata=JSON.parse(this.response); 
    for(vari=0; i<jsondata.length;i++)
 { 
-   console.log(jsondata[i].name+" shares its border with " +jsondata[i].boders.length +" countries that includes "+json[i].borders)
+   console.log(jsondata[i].name+" shares its border with " +jsondata[i].borders.length +" countries that includes "+json[i].borders)
                }
 
 }
