8b93518ce6140d0815da7a8e45d46b9a57b52605
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-06-03 21:09:44 -0700

    Update 3.js

--- Day 3:(03-06-2020)/3.js
+++ Day 3:(03-06-2020)/3.js
@@ -1,1 +1,15 @@
+//3. Write a callback which is called
+//3.1. if the give number is even
 
+function demo(a,check)
+{
+    if(a%2==0)
+    {
+        check()
+        return true;
+    }
+}
+function demo2(){
+    console.log("even")
+}
+demo(2,demo2)
