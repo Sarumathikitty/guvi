7ce7050d9c70317c896b6ecb10feedc5b7bbb603
Sarumathi <54061134+Sarumathikitty@users.noreply.github.com>
2020-06-25 20:18:00 +0530

    Update Contact.js

--- React.js/Router/Contact.js
+++ React.js/Router/Contact.js
@@ -7,9 +7,8 @@ class Contact extends React.Component {
       contactlist: [],
     };
   }
-
   componentDidMount() {
-    fetch("http://localhost:4040/contacts")
+    fetch(" http://localhost:4040/contacts")
       .then((res) => res.json())
       .then((data) => {
         this.setState({ contactlist: data });
@@ -18,30 +17,23 @@ class Contact extends React.Component {
         console.log(err);
       });
   }
-
   render() {
     return (
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th>FIrst Name</th>
-              <th>Last Name</th>
-              <th>Mobile Number</th>
-              <th>Agent Email</th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.contactlist.map((contact, index) => (
-              <tr key={index}>
-                <td>{contact.name.first}</td>
-                <td>{contact.name.last}</td>
-                <td>{contact.mobile}</td>
-                <td>{contact.email}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div>        
+          {this.state.contactlist.map((contact, index) => (          
+              <table>
+                <tr  key={index}>
+                  <th>Name</th>
+                  <th>Mobile number</th>
+                  <th>Email</th>
+                </tr>
+                <tr>
+                  <td>{contact.name.first} {contact.name.last}</td>
+                  <td>{contact.mobile}</td>
+                  <td>  {contact.email}</td>
+                </tr>
+              </table>        
+          ))}        
       </div>
     );
   }
